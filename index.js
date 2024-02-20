bd3cb59b029fe9a6ceb384abf0e1991d2f7cac99
noah-maclean <noahmac5000@hotmail.com>
2024-02-20 22:00:33 +0000

    added more buttons, save their data. change name and tweaked looks

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-let noteActive = false;
-var currentNote;
+// let noteActive = false;
+// var currentNote;
+var activeNote;
+const noteTexts = ["", "", "", "", "", ""];
+const noteSaved = [false, false, false, false, false, false]
+
+const note = document.getElementById("note");
+const noteTitle = document.getElementById("noteTitle");
+const noteText = document.getElementById("noteText");
+const saveMessage = document.getElementById("saveMessage");
 
 function displayNote(currentNote) {
-    document.getElementById("note").style.display = "flex";
-    document.getElementById("noteTitle").textContent = `Note ${currentNote}`;
-    noteActive = true;
+    note.style.display = "flex";
+
+    if (noteSaved[currentNote] === true){
+        // alert("note has been saved");
+        var firstSpace = noteTexts[currentNote].indexOf(" ");
+        // alert(firstSpace)
+        // alert(noteTexts[currentNote].substring(0, firstSpace));
+        noteTitle.textContent = noteTexts[currentNote].substring(0, firstSpace)
+    } else {
+        noteTitle.textContent = `Note ${currentNote}`;
+    }
+    
+    noteText.value = noteTexts[currentNote];
+    saveMessage.style.opacity = 0;
+    // noteActive = true;
+    activeNote = currentNote;
 }
 
 function hideNote() {
-    document.getElementById("note").style.display = "none";
-    noteActive = false;
+    note.style.display = "none";
+    // noteText.value = null;
+    // noteActive = false;
 }
+
+function saveNote() {
+    // document.getElementById("note").style.display = "none";
+    noteTexts[activeNote] = noteText.value;
+    saveMessage.style.opacity = 1;
+    // alert(noteTexts[activeNote]);
+    noteSaved[activeNote] = true;
+    // alert(noteSaved[activeNote])
+    var firstSpace = noteTexts[activeNote].indexOf(" ");
+    document.getElementById(`note${activeNote}`).value = noteTexts[activeNote].substring(0, firstSpace); 
+
+}
+
+// function changeTitle() {
+
+// }
